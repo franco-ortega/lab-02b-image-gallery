aa8ab63cb86fdfcf16878a2c2826cc2c415c9029
franco-ortega <francopdx@gmail.com>
2020-06-09 17:10:37 -0700

    image gallery website and screenshots

---
+++ test/example.test.js
@@ -0,0 +1,16 @@
+// IMPORT MODULES under test here:
+// import example from '../src/example.js';
+
+const test = QUnit.test;
+
+test('time to test a function', function(expect) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+    //Expect
+    // Make expectations about what is expected valid result
+    expect.equal(true, false);
+});
